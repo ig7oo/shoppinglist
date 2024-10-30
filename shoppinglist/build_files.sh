28e4d810ef44e846f2f302bdb0a4214b828f98d5
ig7oo <davidwesterlund012@gmail.com>
2024-10-30 10:25:08 +0200

    vercel

--- shoppinglist/build_files.sh
+++ shoppinglist/build_files.sh
@@ -1,8 +1,8 @@
-echo -m pip install -r requirements.txt
+#!/bin/bash
 
-echo
+python3 -m pip install -r requirements.txt
+
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 
-echo
 python3 manage.py collectstatic --noinput
